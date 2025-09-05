719eeb01753c498b1ae076088107f97f2d53700c
lucas0sk <osakwecloud@gmail.com>
2025-09-05 20:07:41 -0300

    feat(next): added next config for images

--- Symposium-next-js/package/next.config.mjs
+++ Symposium-next-js/package/next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+
+    // Só precisa se você carrega imagens fora de /public.
+    images: {
+        formats: ['image/avif', 'image/webp'],
+        remotePatterns: [
+            { protocol: 'https', hostname: 'i.scdn.co' },
+            { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+            { protocol: 'https', hostname: 'res.cloudinary.com' },
+            { protocol: 'https', hostname: 'images.unsplash.com' },
+        ],
+    },
+};
+
 
 export default nextConfig;
